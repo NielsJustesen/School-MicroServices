2d20647413f87d7425be3e03dc7c8559ad1a29fa
NielsJustesen <niels449@hotmail.com>
2020-02-24 12:59:11 +0100

    test

---
+++ MicroServiceEcoSystem/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroServiceEcoSystem_Framework
+{
+    class Test
+    {
+        private void TestMethod()
+        {
+            string someString = "Some text";
+        }
+    }
+}
